7bbe5eeb7a1f4c30b7ba70a0a1902610628e17dd
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-15 12:55:06 -0700

    Switch to defaultdict strategy and printed output

--- wordcount.py
+++ wordcount.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def count_word(text_file):
     """Counts number of occurrences of each word in `text_file`."""
 
@@ -8,14 +10,22 @@ def count_word(text_file):
         words = text_data.read().replace("\n", " ").split(" ")
     
         # create a dictionary, loop thru each word, check if in dict
-        counts_of_words = {}
-        
+        # counts_of_words = {}
+        counts_of_words = defaultdict(int)
+        # switched from using .get() on a normal dict
+        # to defaultdict -> sets value of a new key to default 0 (for int)
+
         for word in words:
             # already in dict - create + add one, 
             # not already in dict - just add one
-            counts_of_words[word] = counts_of_words.get(word, 0) + 1
+            # counts_of_words[word] = counts_of_words.get(word, 0) + 1
+            counts_of_words[word] += 1
 
-    return counts_of_words
+    del counts_of_words['']
+
+    for word, count in dict(counts_of_words).items():
+        print(word, count)
+
     
 
 count_word("test.txt")
